40f504a4266ef8634ca9799c884b315559399ba0
IntyazB <156741671+IntyazB@users.noreply.github.com>
2024-01-16 11:23:38 +0000

    all is well

---
+++ 01_basics/03_dataType.js
@@ -0,0 +1,34 @@
+"use strict"  // treat all js code as newer version
+
+//alert(3+3);  we are using node.js not browser
+
+console.log(3
+    +3); //this will cause reaadability problem in our code
+
+console.log("mashak");
+
+// 255; // two-hundred and fifty-five
+// 255.0; // same numberno
+// console.log(255 === 255.0); // true
+// console.log(255 === 0xff); // true (hexadecimal notation)
+// console.log(255 === 0b11111111); // true (binary notation)
+// console.log(255 === 0.255e3); // true (decimal exponential notation)
+
+let name1 = "mashak";
+let age = 22;
+let isAge=false;
+
+// DataTypes 1). primitive type and  2). Non primitive type
+// Number
+// BigInt
+// String
+// boolean 
+// null =>  this is standalone value , this defined value but empty object
+// undefined  => this is not defined value that means declaration is done but intialization is not done
+
+//Non primitive type data 
+//Object  
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+console.log(typeof name1);
