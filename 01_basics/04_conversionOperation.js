0c5075da39f62c1e827c6ed9277ea2e2f510a541
IntyazB <156741671+IntyazB@users.noreply.github.com>
2024-01-16 11:18:49 +0000

    conversionOperation

---
+++ 01_basics/04_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = false; o/p=>0
+// let score = true; o/p=>1
+// let score = null; o/p=>0
+let score = "mashak";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let no = Number(score);
+// console.log(typeof no);
+// console.log(no);
+
+let a= 1;
+let a1=Boolean(a);
+console.log(a1);   //o/p true
+
+let a2=123;
+let a3=Boolean(a2);
+console.log(typeof a3);
+console.log(a3);
